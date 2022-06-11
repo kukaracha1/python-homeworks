835ca22ee276fb543d684c6794774bfe200144ec
unknown <hashambardzumyan@gmail.com>
2022-06-11 19:40:40 +0300

    домашка

---
+++ Homework1_final.py
@@ -0,0 +1,42 @@
+print(2)
+
+
+
+#Task1
+y = 7
+p = 2
+d=2*y-p
+print(d)
+
+#Task2
+name = 'John Snow'
+age = str(29)
+print(name,age)
+
+#Task3(работа со строками)
+
+#print("I'm a student and I'll become a "strong" programmer") #I'm a student and I'll become a "strong" programmer
+x = "I'm a student and I'll become a \"strong\" programmer"
+print(x)
+
+
+#print("C:\Users\GoodStudent") #C:\Users\GoodStudent
+b = "C:\\Users\\GoodStudent"
+print(b)
+
+#(x = "My name is Agent Smith" print(x[?]) #y print(x[?:?:?]) #nesgt)
+x = "My name is Agent Smith"
+print(x[1])
+print(x[3:16:3])
+
+
+
+
+
+
+
+
+
+
+
+
